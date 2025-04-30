5b15bb7c99e5ab0a2445054cdce25bc1e1f4eb5b
Coriluz <760351@student.dusd.net>
2025-04-30 11:41:07 -0700

    I'm on 3.4
    I am now starting 3, Add a dirt splatter. Particles align perfectly with death.

--- U3SoundsAndEffectsCoralineP4/Assets/Scripts/PlayerController.cs
+++ U3SoundsAndEffectsCoralineP4/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -14,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         
         
@@ -22,10 +25,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -39,6 +43,9 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("GameOver");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
     }
 }
